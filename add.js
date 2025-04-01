71c0824f7c78c100969fe161028a7718f06a21f6
singhlalima <153114735+singhlalima@users.noreply.github.com>
2025-04-01 10:22:44 +0530

    Add files via upload

---
+++ add.js
@@ -0,0 +1,19 @@
+
+export const add = (a,b)=> 
+{
+    return a+b
+}
+ export const sub = (a,b)=>{
+    return a-b
+}
+
+// console.log(add(33,99));
+
+// let myfunc = (a,b) => {
+//       return a*b
+// }
+
+// console.log(myfunc(33,99));
+
+
+// module.exports = sub;
---
+++ index.js
@@ -0,0 +1,5 @@
+import  {add, sub} from "./add.js"
+// const calc = require("./add.js");
+console.log(add);
+console.log(add(3,4));
+console.log(sub(6,7));
